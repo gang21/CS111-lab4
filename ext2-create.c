afce26594b78684b864c20f0707f51a48b9aa251
Gabriella Ang <gabriella90631@gmail.com>
2023-05-30 13:03:35 -0700

    inode table

--- ext2-create.c
+++ ext2-create.c
@@ -353,19 +353,40 @@ void write_inode_table(int fd) {
 	/* You should add your 3 other inodes in this function and delete this
 	   comment */
 
+	struct ext2_inode root = {0};
+	root.i_mode = EXT2_S_IFDIR
+	                              | EXT2_S_IRUSR
+	                              | EXT2_S_IWUSR
+	                              | EXT2_S_IXUSR
+	                              | EXT2_S_IRGRP
+	                              | EXT2_S_IXGRP
+	                              | EXT2_S_IROTH
+	                              | EXT2_S_IXOTH;
+	root.i_uid = 0;
+	root.i_size = 1024;
+	root.i_atime = current_time;
+	root.i_ctime = current_time;
+	root.i_mtime = current_time;
+	root.i_dtime = 0;
+	root.i_gid = 0;
+	root.i_links_count = 2;
+	root.i_blocks = 2; /* These are oddly 512 blocks */
+	root.i_block[0] = ROOT_DIR_BLOCKNO;
+	write_inode(fd, EXT2_ROOT_INO, &root);
+
 	struct ext2_inode hello_world_inode = {0};
 	hello_world_inode.i_mode = EXT2_S_IFREG
 	                              | EXT2_S_IRUSR
 	                              | EXT2_S_IWUSR
 	                              | EXT2_S_IRGRP
 	                              | EXT2_S_IROTH;
-	hello_world_inode.i_uid = 0;
+	hello_world_inode.i_uid = 1000;
 	hello_world_inode.i_size = 1024;
 	hello_world_inode.i_atime = current_time;
 	hello_world_inode.i_ctime = current_time;
 	hello_world_inode.i_mtime = current_time;
 	hello_world_inode.i_dtime = 0;
-	hello_world_inode.i_gid = 0;
+	hello_world_inode.i_gid = 1000;
 	hello_world_inode.i_links_count = 1;
 	hello_world_inode.i_blocks = 2; /* These are oddly 512 blocks */
 	hello_world_inode.i_block[0] = HELLO_WORLD_INO;
@@ -377,17 +398,17 @@ void write_inode_table(int fd) {
 	                              | EXT2_S_IWUSR
 	                              | EXT2_S_IRGRP
 	                              | EXT2_S_IROTH;
-	hello_inode.i_uid = 0;
+	hello_inode.i_uid = 1000;
 	hello_inode.i_size = 1024;
 	hello_inode.i_atime = current_time;
 	hello_inode.i_ctime = current_time;
 	hello_inode.i_mtime = current_time;
 	hello_inode.i_dtime = 0;
-	hello_inode.i_gid = 0;
+	hello_inode.i_gid = 1000;
 	hello_inode.i_links_count = 1;
 	hello_inode.i_blocks = 2; /* These are oddly 512 blocks */
 	hello_inode.i_block[0] = HELLO_INO;
-	write_inode(fd, HELLO_INO, &hello_inode);
+	write_inode(fd, HELLO_INO, "Hello world\n");
 }
 
 void write_root_dir_block(int fd) {
